5d396e5e8b6d10580c82abae2eafd7c3f5bbef0b
mikedr <derosamiguel@gmail.com>
2022-07-21 08:27:56 -0300

    First commit

---
+++ src/main/java/com/learn/springboot/demo/sbexample/SbexampleApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.demo.sbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/springboot/demo/sbexample/SbexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.demo.sbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
