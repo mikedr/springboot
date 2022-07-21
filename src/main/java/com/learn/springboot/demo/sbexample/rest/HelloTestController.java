5f9152da701c723a74e50a35d87d17ea251e9751
mikedr <derosamiguel@gmail.com>
2022-07-21 08:40:10 -0300

    Adding REST controller

---
+++ src/main/java/com/learn/springboot/demo/sbexample/rest/HelloTestController.java
@@ -0,0 +1,15 @@
+package com.learn.springboot.demo.sbexample.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloTestController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return ("Hello my friend. Time on server is: "+LocalDateTime.now());
+	}
+}
